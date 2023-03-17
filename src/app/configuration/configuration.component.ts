58fc863b2c89d8bb1c010bb60fbfbe6bff3a1eb1
evgenyfrolov <evgeny.frolov@xclinical.com>
2023-03-17 16:01:09 +0100

    upd form

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -1,8 +1,9 @@
-import { Component, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ConfigurationService } from 'infrastructure/configuration.service';
 import { ConfigurationData } from 'infrastructure/configuration-data';
 import { Router } from '@angular/router';
+import { IntervalsClient } from 'infrastructure/intervals.client';
 
 
 @Component({
@@ -10,33 +11,33 @@ import { Router } from '@angular/router';
   templateUrl: './configuration.component.html',
   styleUrls: ['./configuration.component.scss']
 })
-export class ConfigurationComponent {
-  configurationForm: FormGroup = this.getConfigurationForm()
+export class ConfigurationComponent implements OnInit {
+  configurationForm: FormGroup = this.getConfigurationForm();
 
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
-    private configurationService: ConfigurationService
+    private configurationService: ConfigurationService,
+    private intervalsClient: IntervalsClient,
   ) {
   }
 
   ngOnInit(): void {
-    let configuration = this.configurationService.getConfiguration()
+    let configuration = this.configurationService.getConfiguration();
 
     this.configurationForm.setValue({
       athleteId: configuration.athleteId || null,
       apiKey: configuration.apiKey || null,
-    })
+    });
   }
-
 
   onSubmit(): void {
     let newConfiguration = new ConfigurationData(
       this.configurationForm.value.athleteId,
       this.configurationForm.value.apiKey
-    )
+    );
 
-    this.configurationService.setConfiguration(newConfiguration)
+    this.configurationService.setConfiguration(newConfiguration);
     this.router.navigate(['/wellness']);
   }
 
--- src/app/wellness-form/wellness-form.component.ts
+++ src/app/wellness-form/wellness-form.component.ts
@@ -125,6 +125,6 @@ export class WellnessFormComponent implements OnInit {
     this.requestSuccessful = true;
     setTimeout(() => {
       this.requestSuccessful = false;
-    }, 3000);
+    }, 5000);
   }
 }
